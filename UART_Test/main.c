a1848786ed308fad74e432dba29e299fdc603e70
krypto <krypt0pr0xy@mail.ch>
2021-09-16 12:32:35 +0200

    Beginning of UART Config

--- UART_Test/main.c
+++ UART_Test/main.c
@@ -1,44 +1,56 @@
-/*****************************************************************************
-*
-* Copyright (C) 2013 - 2017 Texas Instruments Incorporated - http://www.ti.com/
-*
-* Redistribution and use in source and binary forms, with or without
-* modification, are permitted provided that the following conditions
-* are met:
-*
-* * Redistributions of source code must retain the above copyright
-*   notice, this list of conditions and the following disclaimer.
-*
-* * Redistributions in binary form must reproduce the above copyright
-*   notice, this list of conditions and the following disclaimer in the
-*   documentation and/or other materials provided with the
-*   distribution.
-*
-* * Neither the name of Texas Instruments Incorporated nor the names of
-*   its contributors may be used to endorse or promote products derived
-*   from this software without specific prior written permission.
-*
-* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-* A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-* OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-* SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-* LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-* DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-* THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*
-******************************************************************************
-*
-* MSP432 empty main.c template
-*
-******************************************************************************/
 
+//UART TEST
+#include <stdint.h>
 #include "msp.h"
+
+
 
 int main(void)
 {
+    //INIT UART
+    //Universal Asynchronous Receiver/Transmitter (UART)
+    //Slau 723a page 1620
+
+    //UART PA0 33 – U0Rx
+    //UART PA1 34 – U0Tx
+
+
+    //UART Initialization and Configuration
+    //26.4 page 1629
+    //-----------------------------------------------------------------------
+    //******1. Enable the UART module page 331
+    // UART Module 0 Run Mode Clock Gating Control
+    SYSCTL->RCGCUART |= BIT0;//0x1 = Enable and provide a clock to UART module 0 in run mode
+
+
+    //******2. Enable the clock to the appropriate GPIO module page 326
+    //GPIO Port A Run Mode Clock Gating Control
+    SYSCTL->RCGCGPIO |= BIT0;//0x1 = Enable and provide a clock to GPIO port A in run mode.
+
+
+    //******3.Set the GPIO AFSEL bits for the appropriate pins page 1213
+    //GPIO Alternate Function Select
+    GPIOA->AFSEL |= BIT0;//0x1 = The associated pin functions as a peripheral signal and is controlled by the alternate hardware function.
+    GPIOA->AFSEL |= BIT1;//0x1 = The associated pin functions as a peripheral signal and is controlled by the alternate hardware function.
+
+
+    //******4.Configure the GPIO current level or slew rate as specified for the mode selected
+    //GPIO Slew Rate Control Select
+    GPIOA->SLR |= BIT0;//0x1 = Slew rate control is enabled for the corresponding pin.
+    GPIOA->SLR |= BIT1;//0x1 = Slew rate control is enabled for the corresponding pin.
+
+    //GPIO 12-mA Drive Select
+    GPIOA->DR12R |= BIT0;//The corresponding GPIO pin has 12-mA drive
+    GPIOA->DR12R |= BIT1;//The corresponding GPIO pin has 12-mA drive
+
+
+    //*****5.Configure the PMCn fields in the GPIOPCTL register to assign the UART signals to the appropriate pins
+
+
+    while(1)
+    {
+        __delay_cycles(1000);//NOP
+    }
+
     return 0;
 }
